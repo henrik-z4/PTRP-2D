ec366e68cd2dbd638290bdd2a5501636419e7609
Henrik <muradyango@student.bmstu.ru>
2024-12-03 23:27:00 +0300

    Добавлено Солнце

--- door_chixir.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-@export var target_position: Vector2 = Vector2.ZERO
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "main_character" and GameState.dialogue_finished:
-		pass
